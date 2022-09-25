cc70f69b45f497b91021ee3519d49d21b622d0ae
Kartik Bade <kartikbade19@gmail.com>
2022-09-26 02:30:47 +0530

    Made Tip Calculator using Compose

---
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -0,0 +1,177 @@
+package com.example.birthdaycardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TipTimeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeScreen() {
+    var inputAmount by remember { mutableStateOf("0") }
+    val amount = inputAmount.toDoubleOrNull() ?: 0.0
+
+    var inputTipPercentage by remember { mutableStateOf("0") }
+    val tipPercentage = inputTipPercentage.toDoubleOrNull() ?: 0.0
+
+    var roundUp by remember { mutableStateOf(false) }
+
+    val tip = calculateTip(amount, tipPercentage, roundUp)
+
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .height(32.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField(
+            label = R.string.cost_of_service,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            value = inputAmount,
+            onValueChange = { inputAmount = it }
+        )
+        EditNumberField(
+            label = R.string.tip_percentage,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager.clearFocus()
+                }
+            ),
+            value = inputTipPercentage,
+            onValueChange = { inputTipPercentage = it }
+        )
+        RoundTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, tip),
+            modifier = Modifier.align(CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun EditNumberField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(id = label)) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
+    )
+}
+
+@Composable
+fun RoundTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(id = R.string.round_the_tip)
+        )
+        Switch(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+            colors = SwitchDefaults.colors(
+                uncheckedThumbColor = Color.DarkGray
+            )
+        )
+    }
+}
+
+fun calculateTip(
+    amount: Double,
+    tipPercentage: Double,
+    roundUp: Boolean
+): String {
+    var tip = amount * tipPercentage / 100
+    if (roundUp)
+        tip = ceil(tip)
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    BirthdayCardAppTheme {
+        TipTimeScreen()
+    }
+}
